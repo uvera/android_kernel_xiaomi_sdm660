31add11f66d43c3f36bf5a9be7e7ccf9911ae092
Park Ju Hyung <qkrwngud825@gmail.com>
2018-12-24 01:27:48 +0900

    focaltech: more optimizations
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/input/touchscreen/focaltech_d9_d9p/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_d9_d9p/focaltech_core.c
@@ -40,6 +40,11 @@
 #endif
 #include <linux/proc_fs.h>
 
+/*****************************************************************************
+* Global variable or extern global variabls/functions
+*****************************************************************************/
+static struct fts_ts_data *fts_data;
+
 #include "focaltech_i2c.c"
 #if FTS_GESTURE_EN
 #include "focaltech_gesture.c"
@@ -48,7 +53,7 @@
 #if FTS_LOCK_DOWN_INFO_EN
 char tp_lockdown_info[30];
 #endif
-u8 fts_chipid = 0;
+static u8 fts_chipid = 0;
 #ifdef CONFIG_PM
 static int boot_into_ffbm;
 #endif
@@ -66,21 +71,11 @@ static int boot_into_ffbm;
 #endif
 
 /*
- * Global variable or extern global variabls/functions
- */
-struct fts_ts_data *fts_data;
-
-/*
  * Static function prototypes
  */
-static void fts_release_all_finger(void);
-static int fts_ts_suspend(struct device *dev);
-static int fts_ts_resume(struct device *dev);
-
-#if defined(CONFIG_HQ_HARDWARE_INFO)
-char fts_tpdname[60] = {0};
-char fts_color[20] = {0};
-#endif
+static inline void fts_release_all_finger(void);
+static inline int fts_ts_suspend(struct device *dev);
+static inline int fts_ts_resume(struct device *dev);
 
 /*
  * Name: fts_wait_tp_to_valid
@@ -88,7 +83,7 @@ char fts_color[20] = {0};
  *        need call when reset/power on/resume...
  * Return: return 0 if tp valid, otherwise return error code
  */
-int fts_wait_tp_to_valid(struct i2c_client *client)
+static inline int fts_wait_tp_to_valid(struct i2c_client *client)
 {
 	int ret = 0;
 	int cnt = 0;
@@ -231,7 +226,7 @@ static int fts_get_ic_information(struct fts_ts_data *ts_data)
  * Name: fts_tp_state_recovery
  * Brief: Need execute this function when reset
  */
-void fts_tp_state_recovery(struct i2c_client *client)
+static inline void fts_tp_state_recovery(struct i2c_client *client)
 {
 	FTS_FUNC_ENTER();
 	/* wait tp stable */
@@ -251,7 +246,7 @@ void fts_tp_state_recovery(struct i2c_client *client)
  * Brief: Execute reset operation
  * Input: hdelayms - delay time unit:ms
  */
-int fts_reset_proc(int hdelayms)
+static inline int fts_reset_proc(int hdelayms)
 {
 	FTS_FUNC_ENTER();
 	gpio_direction_output(fts_data->pdata->reset_gpio, 0);
@@ -269,7 +264,7 @@ int fts_reset_proc(int hdelayms)
  * Name: fts_irq_disable
  * Brief: disable irq
  */
-void fts_irq_disable(void)
+static inline void fts_irq_disable(void)
 {
 	unsigned long irqflags;
 
@@ -289,7 +284,7 @@ void fts_irq_disable(void)
  * Name: fts_irq_enable
  * Brief: enable irq
  */
-void fts_irq_enable(void)
+static inline void fts_irq_enable(void)
 {
 	unsigned long irqflags = 0;
 
@@ -536,7 +531,7 @@ static void fts_show_touch_buffer(u8 *buf, int point_num)
  * Name: fts_release_all_finger
  * Brief: report all points' up events, release touch
  */
-static void fts_release_all_finger(void)
+static inline void fts_release_all_finger(void)
 {
 	struct input_dev *input_dev = fts_data->input_dev;
 #if FTS_MT_PROTOCOL_B_EN
@@ -564,7 +559,7 @@ static void fts_release_all_finger(void)
  * Input: events info
  * Return: return 0 if success
  */
-static int fts_input_report_key(struct fts_ts_data *data, int index)
+static inline int fts_input_report_key(struct fts_ts_data *data, int index)
 {
 	u32 ik;
 	int id = data->events[index].id;
@@ -596,7 +591,7 @@ static int fts_input_report_key(struct fts_ts_data *data, int index)
 }
 
 #if FTS_MT_PROTOCOL_B_EN
-static int fts_input_report_b(struct fts_ts_data *data)
+static inline int fts_input_report_b(struct fts_ts_data *data)
 {
 	int i;
 	int touchs = 0;
@@ -661,7 +656,7 @@ static int fts_input_report_b(struct fts_ts_data *data)
 }
 
 #else
-static int fts_input_report_a(struct fts_ts_data *data)
+static inline int fts_input_report_a(struct fts_ts_data *data)
 {
 	int i = 0;
 	int touchs = 0;
@@ -807,7 +802,7 @@ static inline int fts_read_touchdata(struct fts_ts_data *data)
 /*
  * Name: fts_report_event
  */
-static void fts_report_event(struct fts_ts_data *data)
+static inline void fts_report_event(struct fts_ts_data *data)
 {
 #if FTS_MT_PROTOCOL_B_EN
 	fts_input_report_b(data);
@@ -1316,7 +1311,7 @@ int fts_LockDownInfo_get_from_boot(struct i2c_client *client, char *pProjectCode
 }
 #endif
 
-#ifdef CONFIG_HQ_HARDWARE_INFO
+#if 0
 /*
  * Function:
  * Read fw version and config version .
@@ -1688,7 +1683,7 @@ static int fts_ts_remove(struct i2c_client *client)
 /*
  * Name: fts_ts_suspend
  */
-static int fts_ts_suspend(struct device *dev)
+static inline int fts_ts_suspend(struct device *dev)
 {
 	int ret = 0;
 	struct fts_ts_data *ts_data = dev_get_drvdata(dev);
@@ -1730,7 +1725,7 @@ static int fts_ts_suspend(struct device *dev)
 /*
  * Name: fts_ts_resume
  */
-static int fts_ts_resume(struct device *dev)
+static inline int fts_ts_resume(struct device *dev)
 {
 	struct fts_ts_data *ts_data = dev_get_drvdata(dev);
 
