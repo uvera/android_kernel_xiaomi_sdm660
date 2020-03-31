d29ee114b56e17ee79f3197e52601dd298857395
Dušan Uverić <dusan.uveric9@gmail.com>
2020-03-31 12:28:16 +0200

    Revert "CHROMIUM: selinux: Do not log "permissive" denials"
    * Duplicate commit
    
    This reverts commit f276920a9d589d472d1061ed4182debfa4cb2ca3.
    
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -752,10 +752,6 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 {
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
-	
-	/* Only log permissive=1 messages */
-	if (denied && !result)
-		return 0;
 
 	/* Only log permissive=1 messages for SECURITY_SELINUX_DEVELOP */
 	if (denied && !result)
